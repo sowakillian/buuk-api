9e9ce8578d7d3457eb51215c599ad7b645ea0e69
sowakillianpro <sowakillianpro@gmail.com>
2021-09-15 11:09:26 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f85351e7348fd5aac99d7c6716030e85'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cad46811-d910-4214-b20b-0699de3ff554'
+};
